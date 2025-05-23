232aa7ed991aef1cec4ab3ae08ebbea4718c9a8a
iamgroot1123 <2306madhu@gmail.com>
2025-05-23 20:41:32 +0530

    c7

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,12 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16 MB limit
-    app.register_blueprint(main)
     
-    if __name__ == '__main__':
-        port = int(os.environ.get('PORT', 8000))
-        app.run(host='0.0.0.0', port=port)
+    # Configure app
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+    app.config['PORT'] = int(os.environ.get('PORT', 8000))
+    
+    # Register blueprints
+    app.register_blueprint(main)
     
     return app
