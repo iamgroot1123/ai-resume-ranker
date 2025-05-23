0d791f3145802f7cc4d7abe477a4d81c931b099c
iamgroot1123 <2306madhu@gmail.com>
2025-05-23 20:06:29 +0530

    c6

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from .routes import main
+import os
 
 def create_app():
     app = Flask(__name__)
-    # app.config["UPLOAD_FOLDER"] = "uploads"
-    app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024  # 16MB max upload
-    
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16 MB limit
     app.register_blueprint(main)
+    
+    if __name__ == '__main__':
+        port = int(os.environ.get('PORT', 8000))
+        app.run(host='0.0.0.0', port=port)
     
     return app
